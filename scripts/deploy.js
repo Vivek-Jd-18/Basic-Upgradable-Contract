69b9b380435918501ea58c10a0acc220c45109f4
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2022-10-10 19:12:40 +0530

    initial commit in deploy.js file for better commenting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,8 @@
 async function main() {
+  //Box is main base contract 
   const Box = await ethers.getContractFactory("Box")
   console.log("Deploying Box, ProxyAdmin, and then Proxy...")
+  //proxy is Proxy contract(delegate or router ) contact 
   const proxy = await upgrades.deployProxy(Box, [42], { initializer: 'store' })
   console.log("Proxy of Box deployed to:", proxy.address)
 }
