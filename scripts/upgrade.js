3adcf3b910ddf2a169172b24d6154e40f42d7a5e
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2022-10-10 18:59:23 +0530

    changes in upgrade.js file for explaination of the address.

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,6 +1,10 @@
 async function main() {
+
+    const TransparentUpgradeableProxy = "0x87b2f68670735e0657F4374B1B9f448D48735C07";
+    // "TransparentUpgradeableProxy" this address you'll find at your test scan the last 
+    //deployed contract address named "TransparentUpgradeableProxy"   
     const BoxV2 = await ethers.getContractFactory("BoxV2")
-    let box = await upgrades.upgradeProxy("0x87b2f68670735e0657F4374B1B9f448D48735C07", BoxV2)
+    let box = await upgrades.upgradeProxy(TransparentUpgradeableProxy, BoxV2)
     console.log("Your upgraded proxy is done!", box.address)
 }
 
